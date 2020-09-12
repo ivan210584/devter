b5fc941b89da0d90495e2853cfb26e00b4445e00
Equipo <equipo@MacBook-Air-de-test.local>
2020-09-12 14:32:13 -0500

    Primeros pasos con Netx.js

---
+++ pages/timeline/index.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import styles from '../../styles/Timeline.module.css'
+export default function Timeline({ userName }) {
+    return (
+    <>
+        <h1 className={styles.h1}>This is the timeline of {userName}</h1>
+        <Link href='/'>Go home</Link>
+    </>
+    )
+
+}
+
+Timeline.getInitialProps = () => {
+    return fetch('http://localhost:3000/api/hello')
+        .then(res => res.json())
+        .then(response => {
+            console.log(response)
+            const {userName} = response
+            return {userName}
+        })
+}
